e8facd33592070fddf0dae83967996e4f976e574
ram <rampattabhi143@gmail.com>
2023-10-04 13:25:01 +0530

    adding properties

---
+++ properties/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace properties
+{
+
+    class Program
+    {
+        class Person
+        {
+            private String name ;
+
+
+
+            public String Name
+            {
+                get { return this.name; }
+                set { this.name = value; }
+            }
+
+            private int rollNo;
+            public int RollNo
+            {
+                get { return rollNo; }
+                set { rollNo = value; }
+            }
+
+
+
+
+        }
+        static void Main()
+        {
+            Person myObj = new Person();
+            myObj.Name = "ram";
+            Console.WriteLine("name: "+myObj.Name+" roll no: "+ myObj.RollNo);
+        }
+    }
+}
