2e092d315d6ad195632a0be6890d16e63ef2ba03
ram <rampattabhi143@gmail.com>
2023-10-09 13:46:42 +0530

    stacks and queues

---
+++ data structures/array lists/Queues/Queues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+namespace Queues
+{
+    class Queues
+    {
+        public static void Main(string[] args)
+        {
+            Queue qu = new Queue();
+            qu.Enqueue(1);
+            qu.Enqueue(2);
+            qu.Enqueue(3);
+            qu.Enqueue(4);
+            foreach (var item in qu)
+            {
+                Console.WriteLine(item);
+            }
+            qu.Dequeue();
+            qu.Dequeue();
+            foreach(var item in qu)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
